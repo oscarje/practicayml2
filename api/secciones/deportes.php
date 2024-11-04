7932c5875bcc699660af8b4a9beca03168f4116f
oscarje <53665647+oscarje@users.noreply.github.com>
2024-11-04 09:08:47 -0500

    Add files via upload

---
+++ api/secciones/deportes.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$deportes = [
+"titulo" => "UNIVERSITARIO BICAMPEON 2024",
+"autor" => "sistemas@uns",
+"resumen" => "En su centenario, Universitario de Deportes se corono campeon nacional",
+];
+?>
---
+++ api/secciones/formacion.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$formacion = [
+"titulo" => "XXX",
+"autor" => "sistemas@uns",
+"resumen" => "XXX",
+];
+?>
